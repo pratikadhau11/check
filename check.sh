459eb2097a58746507c5bc5b3fb508d86c377f08
Pratik Adhau <padhau@equalexperts.com>
2020-04-03 18:00:09 +0530

    fail with message

--- check.sh
+++ check.sh
@@ -3,6 +3,7 @@ set -e
 statusURL=$(curl "https://eforms.gov.gg/portal/ServiceStatus.asp")
 if [ "$statusURL" == "000 - System running normal" ]
 then
+	echo "agelisys is down.................."
         exit 1;
 else
         exit -1
